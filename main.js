b7055cf3e42ea1ffbb692d19915bcc81e6e65c9d
Robin <me@taminoful.de>
2015-11-12 21:40:58 +0100

    Added main file
    ♥

---
+++ main.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         WhatsSpy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Manuel Bernhard (Fusix) ♥
+// @match        https://web.whatsapp.com/
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+window.onload = function() {
+    Notification.requestPermission();
+    var jq = document.createElement('script');
+    var isFirstClicked = false;
+    jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+    setInterval(function(){
+        if(typeof jQuery != "undefined") {
+            if(isFirstClicked === false && $("#pane-side > div > div > div").length >= 3) {
+                isFirstClicked = true;
+                for(var i = 0; i < $("#pane-side > div > div > div").length; i++) {
+                    if(parseInt($($("#pane-side > div > div > div")[i]).attr("style").split(";")[3].split(" ")[1].trim().replace("%,","")) == 0) {
+                        $("#pane-side > div > div > div:nth-child(" + (i + 1) + ") > div > div").click();
+                    }
+                }
+            }
+            mirEgal();
+        }
+    },500);
+};
+
+var isOnline = false;
+
+function mirEgal() {
+    if($("#main > header > div.chat-body > div.chat-status.ellipsify > span").text() == "online") {
+        if(isOnline === true) return;
+        isOnline = true;
+        var notification = new Notification('WhatsSpy', {
+            icon: $("#main > header > div.chat-avatar > div > img").attr("src"),
+            body:  $("#main > header > div.chat-body > div.chat-main > h2 > span").text() + " ist nun online!"
+        });
+        setTimeout(function(){notification.close();},10000);
+    } else {
+       isOnline = false;
+    }
+}
